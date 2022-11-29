308836d62b435c255a265e6a79feff4b7ddb3dcd
jeevaann1999 <jeevaann1999@gmail.com>
2022-11-29 15:05:28 +0530

    added view photos

---
+++ src/app/view-photos/view-photos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-photos',
+  templateUrl: './view-photos.component.html',
+  styleUrls: ['./view-photos.component.css']
+})
+export class ViewPhotosComponent {
+
+
+photos:any=[]
+}
